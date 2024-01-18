d26561492adaa0ce3285531146f297c7104ae049
yuvrajjangir <yuvijangir78@gmail.com>
2024-01-18 22:34:19 +0530

    done

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const jsonServer = require("json-server")
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+const middleware = jsonServer.defaults();
 const port = 3001;
 
-
+server.use(middleware);
 server.use(router);
 
 
